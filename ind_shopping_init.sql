7b5fd01cf03e8348fd8dfad237bc1f014db11121
unknown <gramajogonzalo@gmail.com>
2024-09-25 23:33:41 -0300

    init update

--- ind_shopping_init.sql
+++ ind_shopping_init.sql
@@ -1,13 +1,28 @@
 -- Crear el schema
 CREATE SCHEMA ind_shopping;
 
+-- Crear tabla "categoria_insumo"
+CREATE TABLE ind_shopping.categoria_insumo (
+  id_categoria_insumo SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(30) NOT NULL,
+  descripcion VARCHAR(150) NOT NULL,
+  PRIMARY KEY (id_categoria_insumo)
+);
+
 -- Crear tabla "insumo"
 CREATE TABLE ind_shopping.insumo (
   id_insumo SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   descripcion VARCHAR(100) NOT NULL,
   stock MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  stock_max MEDIUMINT UNSIGNED,
   stock_min SMALLINT UNSIGNED,
-  PRIMARY KEY (id_insumo)
+  id_categoria_insumo SMALLINT UNSIGNED,
+  PRIMARY KEY (id_insumo),
+  CONSTRAINT fk_insumo_categoria
+		FOREIGN KEY (id_categoria_insumo)
+        REFERENCES ind_shopping.categoria_insumo (id_categoria_insumo)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 -- Crear tabla "maquina"
@@ -36,6 +51,33 @@ CREATE TABLE ind_shopping.proveedor (
   num_calle SMALLINT UNSIGNED NULL,
   firmas_repre JSON,
   PRIMARY KEY (id_proveedor)
+);
+
+-- Crear tabla "firma"
+CREATE TABLE ind_shopping.firma (
+  id_firma SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  pais VARCHAR(20) NULL,
+  moneda VARCHAR(5) NULL,
+  PRIMARY KEY (id_firma)
+);
+
+-- Crear tabla "representacion"
+CREATE TABLE ind_shopping.representacion (
+  id_representacion SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_firma SMALLINT UNSIGNED NOT NULL,
+  id_proveedor SMALLINT UNSIGNED NOT NULL,
+  PRIMARY KEY (id_representacion),
+  CONSTRAINT fk_proveedor_representacion
+		FOREIGN KEY (id_proveedor)
+        REFERENCES ind_shopping.proveedor (id_proveedor)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+  CONSTRAINT fk_firma_representacion
+		FOREIGN KEY (id_firma)
+        REFERENCES ind_shopping.firma (id_firma)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 -- Crear tabla "contacto"
